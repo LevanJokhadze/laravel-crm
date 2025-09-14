191a9555cbe0e05f48d0f05483d8fe1d11f51b2c
lomsa005 <sabalomsadze912@gmail.com>
2025-09-14 04:11:59 +0400

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,13 @@ class AppServiceProvider extends ServiceProvider
             Route::middleware('api')
          ->prefix('api')
          ->group(base_path('routes/api.php'));
+
+         header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: *');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
     }
 }
